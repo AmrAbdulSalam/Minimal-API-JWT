81a1961f393cd65c04767f6827a07407ca0b4996
AmrAbdulSalam <amrsalam@hotmail.com>
2023-12-10 21:49:40 +0200

    Creating Controllers to handle requests

---
+++ Controller/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinimalAPI_JWT.Models;
+using MinimalAPI_JWT.Services;
+
+namespace MinimalAPI_JWT.Controller
+{
+    [ApiController]
+    [Route("/api/authenticate")]
+    public class UserController : ControllerBase
+    {
+        private readonly IJwtTokenGenerator _tokenGenerator;
+
+        public UserController(IJwtTokenGenerator tokenGenerator)
+        {
+            _tokenGenerator = tokenGenerator;
+        }
+
+        [HttpPost]
+        public ActionResult AuthenticateUser(UserDto user)
+        {
+            var token = _tokenGenerator.GenerateToken(user);
+            if (token == null)
+            {
+                return NotFound("No User found ....");
+            }
+            return Ok(token);
+        }
+    }
+}
---
+++ Controller/UsersInfoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MinimalAPI_JWT.Models;
+using MinimalAPI_JWT.Repository;
+using MinimalAPI_JWT.Services;
+
+namespace MinimalAPI_JWT.Controller
+{
+    [ApiController]
+    [Route("api/users")]
+    [Authorize]
+    public class UsersInfoController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public UsersInfoController(IUserRepository userRepository , IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _userRepository = userRepository;
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+        [HttpGet]
+        public ActionResult<List<UserDto>> GetUsers()
+        {
+            var authrization = Request.Headers["Authorization"];
+            string token = authrization[0].Substring(7);
+            
+            if (!_jwtTokenGenerator.ValidateToken(new JwtToken { Token = token}))
+            {
+                return Unauthorized();
+            }
+
+            return Ok(_userRepository.GetAllUsers());
+        }
+    }
+}
