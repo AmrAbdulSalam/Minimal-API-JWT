ef18cd9f4f1301243c50228cf17123c6cac4ba6b
AmrAbdulSalam <amrsalam@hotmail.com>
2023-12-10 20:46:20 +0200

    Configure JWT authentication service

--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
 ﻿using MinimalAPI_JWT.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using MinimalAPI_JWT.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAuthentication(k =>
+{
+    k.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    k.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(p =>
+{
+    var key = Encoding.UTF8.GetBytes(builder.Configuration["Token:Key"]);
+    p.SaveToken = true;
+    p.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,6 +32,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IUserRepository , UserRepository>();
+
+builder.Services.AddTransient<IJwtTokenGenerator , JwtTokenGenerator>();
 
 var app = builder.Build();
 
@@ -21,7 +45,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
